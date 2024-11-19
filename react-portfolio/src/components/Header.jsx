a66af6b04f3133c422031d53dfcf51540d1d8f92
Robby <robby@henderson-woods.com>
2024-11-18 19:43:00 -0600

    basic footer and header created...edit later if neccesary

---
+++ react-portfolio/src/components/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => (
+    <header className="bg-dark text-white py-3">
+        <div className="container">
+            <h1 className="mb-0">My Portfolio</h1>
+            <nav>
+                <ul className="nav">
+                    <li className="nav-item">
+                        <Link className="nav-link text-white" to="/">About Me</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link text-white" to="/portfolio">Portfolio</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link text-white" to="/contact">Contact</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link text-white" to="/resume">Resume</Link>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+);
+
+export default Header;
